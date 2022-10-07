17551070899cb8e253feb0b1c794e0a9d14afcbc
Aurelia Segarra <av.segarra@gmail.com>
2022-10-07 16:14:08 +0200

    Class Plagecd complete

--- src/datas/Plagecd.java
+++ src/datas/Plagecd.java
@@ -1,41 +1,88 @@
 package datas;
 
+/**
+ * This class defines a music track belonging to a CD. A track has a duration and can be played.
+ * It is characterized by a title (title of the track) and an interpreter(s) (singer, musicians, ...)
+ * @author Aurelia Segarra
+ */
 public class Plagecd {
+    /**
+     * Track duration
+     */
     private Duree laDuree;
+
+    /**
+     * Track title
+     */
     private String leTitre;
+
+    /**
+     * Track interpreter
+     */
     private String lInterprete;
 
 
+    /**
+     * Constructor for a "plage" corresponding to a music track
+     * @param duree Track Duration (⚠️!type Duree)
+     * @param titre Track title
+     * @param interprete Track interpreter
+     */
     public Plagecd (Duree duree, String titre, String interprete){
         this.laDuree = duree;
         this.leTitre = titre;
         this.lInterprete = interprete;
     }
 
+    /**
+     * GETTER
+     * @return duration (⚠️type Duree)
+     */
     public Duree getLaDuree(){
         return laDuree;
     }
 
+    /**
+     * GETTER
+     * @return title
+     */
     public String getLeTitre(){
         return this.leTitre;
     }
 
+    /**
+     * GETTER
+     * @return interpreter
+     */
     public String getLInterprete(){
         return this.lInterprete;
     }
 
+    /**
+     * Method giving the complete description of the track :
+     * <ul>
+     *     <li>Description
+     *     <li>Title
+     *     <li>Interpreter
+     *     <li>Duration
+     * </ul>
+     * @return this description
+     */
     public String getFicheComplete(){
 
         return "Description de la plage de musique\n"
                 + "Titre : " + this.leTitre + "\n"
                 + "Interprete : " + this.lInterprete + "\n"
-                + "Duree : " + this.laDuree;
+                + "Duree : " + this.laDuree.enTexte('S');
     }
 
 
+    /**
+     * Method giving a textual summary : title-interpreter-duration(SSS.MMM sec)
+     * @return this summary
+     */
     @Override
     public String toString(){
-
-        return getLeTitre() + " - " + getLInterprete() + " - " + laDuree;
+        return getLeTitre() + " - " + getLInterprete() + " - " + laDuree.enTexte('S');
     }
 }
